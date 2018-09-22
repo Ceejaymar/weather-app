6971e33fb24bc51826774daaf91368a4ab56d7ac
Carlos Martinez <martinezcj2@gmail.com>
2018-09-22 16:31:10 -0400

    Add config template

--- src/demo-config-key.js
+++ src/demo-config-key.js
@@ -1,4 +1,4 @@
 const clientId = 'Your client id key goes here';
-const clientSecret = 'Your client secret ky goes here';
+const clientSecret = 'Your client secret key goes here';
 
 export { clientId, clientSecret };
